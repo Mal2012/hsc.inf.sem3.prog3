774c85ef1a5c114e47f0f6e84306f54f80daca72
Konstantin Jung <kj88@gmx.net>
2013-10-09 12:51:42 +0200

    08-10-13-12-51

---
+++ semesterProjectC++/project_src/Test.h
@@ -0,0 +1,17 @@
+/*
+ * Test.h
+ *
+ *  Created on: 09.10.2013
+ *      Author: kjung
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+class Test {
+public:
+	Test();
+	virtual ~Test();
+};
+
+#endif /* TEST_H_ */
